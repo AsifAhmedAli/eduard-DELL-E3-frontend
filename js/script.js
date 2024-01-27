ce230f27ed68488907a20e6c4f5b873d678e2a35
Sameer <iamsameerulhaq@gmail.com>
2024-01-27 18:00:18 +0500

    multiple changes

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ fetch("components/header.html")
     document.getElementsByTagName("header")[0].innerHTML = data;
   });
 
-
 // Load Footer Component
 fetch("components/footer.html")
   .then((response) => response.text())
@@ -13,4 +12,9 @@ fetch("components/footer.html")
     document.getElementsByTagName("footer")[0].innerHTML = data;
   });
 
-
+// Load Reviews Section
+fetch("components/reviews-section.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("reviews-section").innerHTML = data;
+  });
